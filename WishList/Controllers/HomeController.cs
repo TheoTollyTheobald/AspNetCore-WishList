eb1df3b3e13daa5391625c71072914e35143e875
TheoTollyTheobald <Theo.Theobald@nottscc.gov.uk>
2022-06-30 13:17:51 +0100

    Part3At1

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(IActionResult index)
+        {
+            return View(index);
+        }
+
+        public IActionResult Error(IActionResult error)
+        {
+            return View(error);
+        }
+    }
+}
